2c7dfbb8ecd55ac942349116660f393c14e38191
Louise <louisepeiffer@hotmail.com>
2021-01-11 15:39:05 +0100

    fdp

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+// Exo 1
+// let chiffre = parseInt(prompt("Choisissez une chiffre"))
+// let compt = 0 
+// while (compt <= chiffre) {
+//     console.log(compt);
+//     compt++
+// }
+
+// Exo 2
+// let fdp = 0
+// while (fdp <= 10) {
+//     console.log(fdp);
+//     fdp++
+// }
+
+// let fdp2 = 3
+// while (fdp2 <= 6) {
+//     console.log(fdp2);
+//     fdp2++
+// }
+
+// let fdp3 = 0
+// while (fdp3 <= 25) {
+//     console.log(fdp3);
+//     fdp3+=2
+// }
